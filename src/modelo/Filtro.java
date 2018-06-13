6ba474ae3eee9d9d8dd1b7a1cc12006d3ca2c9c4
LN710Q <LN710Q@DEILABO1-PC19>
2018-06-13 12:39:34 -0600

    interfaz :c

---
+++ src/modelo/Filtro.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author LN710Q
+ */
+public class Filtro {
+    private int id;
+    private String codigo;
+    private String marca;
+    private int stock;
+    private boolean existencia;
+    public boolean getExistencia;
+
+    public Filtro() {
+    }
+
+    public Filtro(int id, String codigo, String marca, int stock, boolean existencia) {
+        this.id = id;
+        this.codigo = codigo;
+        this.marca = marca;
+        this.stock = stock;
+        this.existencia = existencia;
+    }
+
+    public Filtro(String codigo, String marca, int stock, boolean existencia) {
+        this.codigo = codigo;
+        this.marca = marca;
+        this.stock = stock;
+        this.existencia = existencia;
+    }
+
+    public Filtro(String marca, int stock, boolean existencia) {
+        this.marca = marca;
+        this.stock = stock;
+        this.existencia = existencia;
+    }
+
+    public Filtro(int aInt) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public String getCodigo() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public String getMarca() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public int getStock() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public boolean getExistencia() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
